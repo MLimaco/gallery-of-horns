cf4b0bc767282de359351303d2c1c4bac10b5944
MICHAEL LIMACO <limaco.michael@gmail.com>
2024-11-12 01:13:35 -0500

    update lab02

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,16 +1,27 @@
 import HornerBeast from './HornerBeast'
 import data from '../data.json'
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
 
 function Gallery() {
     return (
         <>
-            <HornerBeast title={"Unicornio"} imageUrl={"https://images.unsplash.com/photo-1550747528-cdb45925b3f7?q=80&w=1978&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"} description={"Este es un Unicornio"} />
-            <HornerBeast title={"Rinoceronte"} imageUrl={"https://images.unsplash.com/photo-1623766982280-a1b03854bdd7?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"} description={"Este es un Rinoceronte"} />
-            {
-                data.map(beast => (
-                    <HornerBeast key={beast._id} title={beast.title} imageUrl={beast.image_url} description={beast.description} />
-                ))
-            }
+            <Container>
+                <Row xs={2} sm={3} md={4} lg={5}>
+                    {
+                        data.map(beast => (
+                            <HornerBeast key={beast._id}
+                                title={beast.title}
+                                imageUrl={beast.image_url}
+                                description={beast.description}
+                            />
+                        ))
+                    }
+                </Row>
+            </Container>
+
         </>
     )
 }
--- src/components/HornerBeast.jsx
+++ src/components/HornerBeast.jsx
@@ -1,9 +1,26 @@
+import { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+
+
 function HornerBeast(props) {
+    const [favorito, setFavorito] = useState(0);
+
+    const addFavorito = () => {
+        setFavorito(favorito + 1)
+    };
     return (
         <>
-            <h2>{props.title}</h2>
-            <img src={props.imageUrl} alt={props.description} title={props.title} />
-            <p>{props.description}</p>
+            <Card style={{ width: '18rem' }}>
+                <Card.Img variant="top" src={props.imageUrl} onClick={addFavorito} />
+                <Card.Body>
+                    <Card.Title>{props.title}</Card.Title>
+                    <Card.Text>
+                        {props.description}
+                    </Card.Text>
+                    <Button variant="primary">❤️ Favorito: {favorito}</Button>
+                </Card.Body>
+            </Card>
         </>
     )
 }
